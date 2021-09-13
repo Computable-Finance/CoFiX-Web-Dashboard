de07630fb0ef204de94f7612ddace5f47fa0cf62
tunogya <tunogya@qq.com>
2021-09-13 14:54:11 +0800

    add index

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,13 +1,26 @@
 import './styles'
 
 import {FC} from 'react'
+import {Link} from "react-router-dom";
+import {CoFiXLogoWithText} from "../../components/Icon";
+import {MenuButton} from "../shared/Menu";
 
 const Index: FC = () => {
-  const classPrefix = 'cofi-page-mining'
+  const classPrefix = 'cofi-page-index'
 
   return (
-    <div className={`cofi-page ${classPrefix}`}>
-      Index
+    <div className={`${classPrefix}`}>
+      <header className={'cofi-header'}>
+        <div className={`cofi-header-prefix`}>
+          <Link to="/">
+            <CoFiXLogoWithText/>
+          </Link>
+        </div>
+        <div className={`cofi-header-extra`}>
+          <Link to={'/pool'}>Dashboard</Link>
+          <MenuButton/>
+        </div>
+      </header>
     </div>
   )
 }
