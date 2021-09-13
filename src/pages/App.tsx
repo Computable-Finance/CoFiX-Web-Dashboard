a1f69c447b51ff4ec9c6afe291b0b544ab1b4c5c
tunogya <tunogya@qq.com>
2021-09-13 14:05:01 +0800

    delete toast

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,6 @@
 import loadable from '@loadable/component'
 import React from 'react'
 import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
 import Header from 'src/pages/shared/Header'
 
 
@@ -13,8 +12,6 @@ const ID = loadable(() => import('./ID'))
 function App() {
   return (
     <main>
-      <ToastContainer />
-
       <Router>
         <Header />
 
