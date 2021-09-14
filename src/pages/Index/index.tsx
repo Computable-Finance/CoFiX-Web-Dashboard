1c9ed64c227bae8f6a41c415ecda15955010b3cb
tunogya <tunogya@qq.com>
2021-09-14 13:35:36 +0800

    add white logo

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -2,27 +2,32 @@ import './styles'
 
 import {FC} from 'react'
 import {Link} from "react-router-dom";
-import {CoFiXLogoWithText} from "../../components/Icon";
+import {CoFiXLogoWithTextWhite} from "../../components/Icon";
 import {MenuButton} from "../shared/Menu";
+import Button from "../../components/Button";
 
 const Index: FC = () => {
   const classPrefix = 'cofi-page-index'
 
   return (
-    <div className={`cofi-page ${classPrefix}`}>
+    <div className={`${classPrefix}`}>
       <header className={'cofi-header'}>
         <div className={`cofi-header-prefix`}>
           <Link to="/">
-            <CoFiXLogoWithText/>
+            <CoFiXLogoWithTextWhite/>
           </Link>
         </div>
         <div className={`cofi-header-extra`}>
-          <Link to={'/pool'}>Dashboard</Link>
+          <Link to={'/pool'}>
+            <div style={{ color: "white" }}>Dashboard</div>
+          </Link>
           <MenuButton/>
         </div>
       </header>
 
-
+      <div className={'cofi-body'}>
+        <Button>App</Button>
+      </div>
 
     </div>
   )
